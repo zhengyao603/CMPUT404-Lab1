a22b2ea58976dfae0ce8a7bfeed3d2e7c39bd31e
zhengyao603 <zhengyao@ualberta.ca>
2021-09-06 02:28:52 -0600

    update

--- script.py
+++ script.py
@@ -2,5 +2,8 @@ import requests
 
 print("The version of the requests is: %s" % requests.__version__)
 
-r = requests.get('http://www.google.com/')
-print(r)
+request1 = requests.get('http://www.google.com/')
+request2 = requests.get('https://raw.githubusercontent.com/zhengyao603/CMPUT404-Lab1/main/script.py')
+
+print(request1)
+printt(request2.text)
